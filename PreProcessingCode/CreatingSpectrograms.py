c73efb334beecb4509b564d9426bbe4b6d7caad5
Katherine Frields <119017028+katydidkat@users.noreply.github.com>
2024-04-19 17:13:53 -0700

    Update CreatingSpectrograms.py

--- PreProcessingCode/CreatingSpectrograms.py
+++ PreProcessingCode/CreatingSpectrograms.py
@@ -1,15 +1,9 @@
 #code source: https://www.tensorflow.org/tutorials/audio/simple_audio#convert_waveforms_to_spectrograms
-import os
-import pathlib
 
 import matplotlib.pyplot as plt
 import numpy as np
-import seaborn as sns
 import tensorflow as tf
 
-from tensorflow.keras import layers
-from tensorflow.keras import models
-from IPython import display
 
 
 def get_audio_tensor(file_path):
