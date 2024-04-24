e4ed40e7d2c1704c2c0f54bee9d1e4cd1ef1b985
Neema Jafari <neemaj@uci.edu>
2024-04-24 14:34:10 -0700

    Connected methods in RetreivingAudioFiles

--- PreProcessingCode/main.py
+++ PreProcessingCode/main.py
@@ -1,14 +1,27 @@
 import CreatingSpectrograms
 import RetreivingAudioFiles
+import DataAugmentation
+import ThresholdMasking
 import tensorflow_io as tfio
+
+folder_path = 'INSERT FOLDER PATH HERE CONTAINING ALL BIRD AUDIO FILES'
+
 
 
 
 def preprocess():
-        '''
-        Method used for preprocessing
-        '''
-        
+    '''
+    Method used for preprocessing
+    '''
+    audio_dict = RetreivingAudioFiles.get_bird_audio_dict(folder_path)
+    audio_tensor_dict = dict()
+
+    for key in audio_dict:
+        audio_tensor_dict[key] = list()
+        file_list = audio_dict[key]
+        for file in file_list:
+            audio_tensor_dict[key].append(RetreivingAudioFiles.get_audio_tensor(file))
+
 
 def main():
         preprocess()
