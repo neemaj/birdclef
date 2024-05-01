c1d46ffc07490661559fa3d9f20bdcd874fe23b7
NeemaJ <87088859+NeemaJ@users.noreply.github.com>
2024-05-01 16:27:59 -0700

    Removed unnecessary import

--- PreProcessingCode/NoiseReduction.py
+++ PreProcessingCode/NoiseReduction.py
@@ -1,7 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
-import tensorflow_probability as tfp
 from scipy.ndimage import binary_erosion, binary_dilation,generate_binary_structure
 
 
