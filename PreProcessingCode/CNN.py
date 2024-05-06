df593bb2b838a12826940f59fa84182e5eb9e27f
Katherine Frields <119017028+katydidkat@users.noreply.github.com>
2024-05-05 20:00:09 -0700

    Update CNN.py

--- PreProcessingCode/CNN.py
+++ PreProcessingCode/CNN.py
@@ -21,19 +21,19 @@ def run_small_model(X_train, y_train, X_valid, y_valid):
     '''
 
     model = Sequential()
-    model.add(Conv2D(64, kernel_size = (5,5), strides = (2,1), input_shape = (256, 128, 1), activation = "relu"))
+    model.add(Conv2D(64, kernel_size = (5,5), strides = (2,1), input_shape = (4, 256, 128, 1), activation = "relu"))
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
-    model.add(Conv2D(64, kernel_size = (5,5), strides = (1,1), input_shape = (64, 64,1), activation = "relu"))
+    model.add(Conv2D(64, kernel_size = (5,5), strides = (1,1), input_shape = (4, 64, 64,1), activation = "relu"))
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
-    model.add(Conv2D(128, kernel_size = (5,5), strides = (1,1), input_shape = (64, 64,1), activation = "relu"))
+    model.add(Conv2D(128, kernel_size = (5,5), strides = (1,1), input_shape = (4, 64, 64,1), activation = "relu"))
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
-    model.add(Conv2D(256, kernel_size = (5,5), strides = (1,1), input_shape = (64, 64,1), activation = "relu"))
+    model.add(Conv2D(256, kernel_size = (5,5), strides = (1,1), input_shape = (4, 64, 64,1), activation = "relu"))
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
-    model.add(Conv2D(256, kernel_size = (3,3), strides = (1,1), input_shape = (64, 64,1), activation = "relu"))
+    model.add(Conv2D(256, kernel_size = (3,3), strides = (1,1), input_shape = (4, 64, 64,1), activation = "relu"))
     model.add(MaxPooling2D(pool_size=(2, 2)))
 
     model.add(Dense(1024, activation='relu'))
