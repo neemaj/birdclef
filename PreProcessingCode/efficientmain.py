cead712014af349d9c2215baa5071f3be0970548
Katherine Frields <frieldskatherine@gmail.com>
2024-05-19 14:50:25 -0700

    Deleting unnecessary functions from efficient main

--- PreProcessingCode/efficientmain.py
+++ PreProcessingCode/efficientmain.py
@@ -32,93 +32,11 @@ chunk_length = 512
 noise_reduce_factor = 0.4
 debug_mode = True
 
-def get_spectrogram_dict():
-    '''
-    Method used to get dictionary of spectrograms from folder_path
-    '''
-    audio_dict = get_bird_audio_dict(folder_path)
-    spectrogram_dict = dict()
-
-    #get the audio tensors, then get the spectrograms
-    #loops through folders
-    for key in audio_dict:
-        spectrogram_dict[key] = list()
-        file_list = audio_dict[key]
-        for file in file_list:
-            tensor = get_audio_tensor(str(file))
-            spectrogram_dict[key].append(get_spectrogram(tensor))
-
-    return spectrogram_dict
 
 
-def separate_noise(spectrogram_dict):
-    '''
-    Method used to get noise and signal
-
-        Returns:
-            Dictionary of signal, and list of noise, as a tuple
-    '''
-    noise_list = list()
-    signals_dict = dict()
-
-    for key in spectrogram_dict:
-         signals_dict[key] = list()
-         spectrogram_list = spectrogram_dict[key]
-         for spec in spectrogram_list:
-              #min max scale
-              mm_spec = min_max_scale_spectrogram(spec).numpy()
-
-
-              #mask
-              signal_spec, noise_spec = mask(mm_spec)
-
-              #erode/dilate
-              binary_signal_spec = apply_binary_erosion_and_dilation_spectrogram(signal_spec)
-              signal_indicator = get_slices_indicator(binary_signal_spec)
-              signal_dilated_indicator = apply_binary_dilation_indicator(signal_indicator)
-
-
-
-              binary_noise_spec = apply_binary_erosion_and_dilation_spectrogram(noise_spec)
-              noise_indicator = get_slices_indicator(binary_noise_spec)
-              noise_dilated_indicator = apply_binary_dilation_indicator(noise_indicator)
-              noise_dilated_indicator_inverted = np.invert(noise_dilated_indicator)
-
-              #isolated
-              log_scale_spec = log_scale_spectrogram(spec).numpy()
-              isolated_signal_spec = get_isolated_spectrogram(signal_dilated_indicator, log_scale_spec)
-              isolated_noise_spec = get_isolated_spectrogram(noise_dilated_indicator_inverted, log_scale_spec)
-
-              #add to respective data structures
-              noise_list.append(isolated_noise_spec)
-              signals_dict[key].append(isolated_signal_spec)
-
-
-
-
-    return signals_dict, noise_list
-    
-    
 
 
      
-
-def preprocess():
-    '''
-    Method used for preprocessing, returns X_train and y_train
-    '''
-    spectrogram_dict = get_spectrogram_dict()
-    signals_dict, noise_list = separate_noise(spectrogram_dict)
-    X_train, y_train = augment(signals_dict, noise_list)
-
-    #testing
-    if debug_mode:
-        print(np.shape(X_train))
-        print(np.shape(y_train))
-        plot_abs_spectrogram(X_train[2], 'Final Test')
-        print(y_train[2])
-
-    return X_train, y_train
 
 
 
