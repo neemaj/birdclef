72c6b03a6e13c60f54c4568b36236cb69af6fbc6
Katherine Frields <119017028+katydidkat@users.noreply.github.com>
2024-07-30 18:36:43 -0700

    Update CNN.py
    Changed batch_size in final model to a paramater rather than hard coding when calling generator

--- PortfolioCode/CNN.py
+++ PortfolioCode/CNN.py
@@ -16,124 +16,29 @@ is_neema_mac = True
 is_neema = True
 
 
-def run_small_model(X_train, y_train, X_valid, y_valid):
+
+
+
+def run_final_model_1(X_train, X_valid, label_dict, model_save_path, number_of_classes, batch_size):
     '''
     Params:
-        X_train: 3D numpy arrays of all final spectrograms (each as its own 2D array) we're training on
-        y_train: corresponding labels (idk if they should be strings or ints) 
+        X_train: list of paths to npy files corresponding to training spectrograms
+        X_valid: list of paths to npy files corresponding to validation spectrograms
         
-        X_test: 3D numpy arrays of all final spectrograms (each as its own 2D array) we're training on
-        y_test: corresponding labels (idk if they should be strings or ints) 
+        label_dict: Dictionary mapping file paths to one-hot sparse encoded label values
+
+        model_save_path: where to save the model to
+
+        number_of_classes: number of classes (species of birds)
+
+        batch_size: batch size for generators
         
     Returns:
 
     '''
-    
-
-    model = Sequential()
-    model.add(Conv2D(64, kernel_size = (5,5), strides = (2,1), input_shape = (512, 256, 1), activation = "relu"))
-    model.add(MaxPooling2D(pool_size=(2, 2), input_shape = (256, 256, 1)))
-
-    model.add(Conv2D(64, kernel_size = (5,5), strides = (1,1), input_shape = (256, 256, 1), activation = "relu"))
-    model.add(MaxPooling2D(pool_size=(2, 2), input_shape = (256, 256, 1)))
-
-    model.add(Conv2D(128, kernel_size = (5,5), strides = (1,1), input_shape = (256, 256, 1), activation = "relu"))
-    model.add(MaxPooling2D(pool_size=(2, 2), input_shape = (256, 256, 1)))
-
-    model.add(Conv2D(256, kernel_size = (5,5), strides = (1,1), input_shape = (256, 256, 1), activation = "relu"))
-    model.add(MaxPooling2D(pool_size=(2, 2), input_shape = (256, 256, 1)))
-
-    model.add(Conv2D(256, kernel_size = (3,3), strides = (1,1), input_shape = (256, 256, 1), activation = "relu"))
-    model.add(MaxPooling2D(pool_size=(2, 2), input_shape = (256, 256, 1)))
-    
-    model.add(Flatten())
-    
-    model.add(Dense(1024, activation='relu'))
-    model.add(Dense(X_train.shape[0], activation='softmax'))
-
-    model.compile(loss=keras.losses.SparseCategoricalCrossentropy(), optimizer=keras.optimizers.Nadam(learning_rate = 0.1), 
-                  metrics=[keras.metrics.SparseCategoricalAccuracy()])
-    
-    model.fit(X_train,y_train, epochs = 10)
-
-
-def run_small_gen_model(X_train, X_valid, label_dict):
-    '''
-    Params:
-        X_train: 3D numpy arrays of all final spectrograms (each as its own 2D array) we're training on
-        y_train: corresponding labels (idk if they should be strings or ints) 
-        
-        label_dict: dictionary with path keys and label values
-        
-    Returns:
-
-    '''
-    # Generators
-    training_generator = Bird_Data_Generator(X_train, label_dict, batch_size=8)
-    validation_generator = Bird_Data_Generator(X_valid, label_dict, batch_size=3)
-
-    # Design model
-    model = Sequential()
-
-    
-    model.add(keras.Input(batch_size = training_generator.batch_size, shape=(512, 256, 1)))
-    model.add(keras.layers.Dropout(.2))
-    
-    model.add(keras.layers.BatchNormalization())
-
-    model.add(Conv2D(64, kernel_size = (5,5), strides = (2,1), activation = "relu"))
-    model.add(MaxPooling2D(pool_size=(2, 2), input_shape = (256, 256, 1)))
-
-    model.add(keras.layers.BatchNormalization())
-    model.add(Conv2D(64, kernel_size = (5,5), strides = (1,1), input_shape = (256, 256, 1), activation = "relu"))
-    model.add(MaxPooling2D(pool_size=(2, 2), input_shape = (256, 256, 1)))
-
-    model.add(keras.layers.BatchNormalization())
-    model.add(Conv2D(128, kernel_size = (5,5), strides = (1,1), input_shape = (256, 256, 1), activation = "relu"))
-    model.add(MaxPooling2D(pool_size=(2, 2), input_shape = (256, 256, 1)))
-
-    model.add(keras.layers.BatchNormalization())
-    model.add(Conv2D(256, kernel_size = (5,5), strides = (1,1), input_shape = (256, 256, 1), activation = "relu"))
-    model.add(MaxPooling2D(pool_size=(2, 2), input_shape = (256, 256, 1)))
-
-    model.add(keras.layers.BatchNormalization())
-    model.add(Conv2D(256, kernel_size = (3,3), strides = (1,1), input_shape = (256, 256, 1), activation = "relu"))
-    model.add(MaxPooling2D(pool_size=(2, 2), input_shape = (256, 256, 1)))
-    
-    
-    model.add(Flatten())
-    
-    model.add(keras.layers.BatchNormalization())
-    model.add(Dense(1024, activation='relu'))
-    model.add(keras.layers.Dropout(.4))
-    model.add(Dense(np.array(X_train).shape[0], activation='softmax'))
-    
-
-    # Train model on dataset
-    
-    
-    model.compile(loss=keras.losses.SparseCategoricalCrossentropy(), optimizer=keras.optimizers.Nadam(learning_rate = 0.1), 
-                  metrics=[keras.metrics.SparseCategoricalAccuracy()])
-    
-    model.fit(training_generator, batch_size =training_generator.batch_size,  validation_data=validation_generator)
-
-
-
-
-def run_final_model_1(X_train, X_valid, label_dict, model_save_path, number_of_classes):
-    '''
-    Params:
-        X_train: 3D numpy arrays of all final spectrograms (each as its own 2D array) we're training on
-        y_train: corresponding labels (idk if they should be strings or ints) 
-        
-        label_dict: dictionary with path keys and label values
-        
-    Returns:
-
-    '''
-    # Generators
-    training_generator = Bird_Data_Generator(X_train, label_dict, batch_size=8)
-    validation_generator = Bird_Data_Generator(X_valid, label_dict, batch_size=8)
+    #Generators are used to give a batch of npy arrays for stochastic gradient descent function
+    training_generator = Bird_Data_Generator(X_train, label_dict, batch_size)
+    validation_generator = Bird_Data_Generator(X_valid, label_dict, batch_size)
 
     # Design model
     model = Sequential()
